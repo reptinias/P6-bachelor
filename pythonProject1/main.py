1a1bda1a17cd092d3a53efe4fed5db7a91caaf09
reptinias <32259405+reptinias@users.noreply.github.com>
2022-04-06 13:16:53 +0200

    Combining point clouds
    Combining point clouds samt omdanne cad til point cloud. Der kan også måles distancen mellem de to point clouds, hvilket can bruges til groud truth

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,20 +1,57 @@
 import open3d as o3d
 import numpy as np
-import os
+from os import listdir
+from os.path import isfile, join
 
+
+# Find all files in folder that contains the laser scans
+onlyfiles = [f for f in listdir("STLS") if isfile(join("STLS", f))]
+
+# Create the array that will contain the combined vertices of the stl models
+pcdArray = np.array([[0,0,0]])
+
+
+# Find the vertices for each stl file and save the vertices in an array
+# Concatenate the saved vertices and combined vertices array
+for x in onlyfiles:
+    foundVertices = np.asarray(o3d.io.read_triangle_mesh("STLS/" + x).vertices)
+    pcdArray = np.concatenate((pcdArray, foundVertices), axis=0)
+
+
+# Create an empty point cloud object and add the vertices to the point cloud as positions
+pcd = o3d.geometry.PointCloud()
+pcdArray = np.delete(pcdArray, 0, axis=0)
+pcd.points = o3d.utility.Vector3dVector(pcdArray)
+
+
+# Read the ground truth Cad model
+Cad = o3d.io.read_triangle_mesh("FullCad.stl")
+# Convert the Cad model into a point cloud with equal amount of points as the scanned point cloud
+CadPCD = Cad.sample_points_uniformly(len(pcdArray))
+
+
+# Calculate distances between pcd and CadPCD.
+pcdist = pcd.compute_point_cloud_distance(CadPCD)
+
+# pcdist is an Open3d object, we need to convert it to a numpy array to access the data
+pcdist = np.asarray(pcdist)
+print(pcdist)
+
+# Remove point that is further than x away from closest neighbor
+ind = np.where(pcdist < 10)[0]
+endPCD = pcd.select_by_index(ind)
+
+# Visualize the two point clouds
+o3d.visualization.draw_geometries([endPCD])
+
+# Save the scanned point cloud
+#o3d.io.write_point_cloud("newPointcloud.ply", endPCD, write_ascii=True, compressed=False, print_progress=True)
+
+
+# vvv Skal højst sandsynligt slettes det her under vvv
 saveMesh = False
 algorithmIndex = 2
 alpha = 0.3
-
-# load point cloud
-pcd = o3d.io.read_point_cloud("20211007_142026_012345_gt.ply")
-
-o3d.visualization.draw_geometries([pcd],
-                                  #zoom = 0.3412,
-                                  #front = [0.4257, -0.2125, -0.8795],
-                                  #lookat = [2.6182, 2.0475, 1.532],
-                                  #up = [-0.0694, -0.9768, 0.2024]
-                                  )
 
 # voxel downsample af point cloud
 downpcd = pcd.voxel_down_sample(voxel_size=0.05)
