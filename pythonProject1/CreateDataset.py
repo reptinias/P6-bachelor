a7b7a8380ad82a2e96cc82a1ca5c29c84b5be360
reptinias <32259405+reptinias@users.noreply.github.com>
2022-05-09 18:51:30 +0200

    Create data + seg files

--- pythonProject1/CreateDataset.py
+++ pythonProject1/CreateDataset.py
@@ -10,13 +10,11 @@ from os.path import isfile, join
 groundTruthList = []
 
 # Load all folders in dataset
-AllFiles = os.listdir("Dataset")
+AllFiles = os.listdir("InropaFiles")
 
 def groundTruth(skipIndex=None):
-    shutil.rmtree('GroundTruth')
-    shutil.rmtree('CleanDataset')
-    shutil.rmtree('CleanPCD')
-    os.mkdir("GroundTruth")
+    os.mkdir("GroundTruthPCD")
+    os.mkdir("GroundTruthPTS")
     print(AllFiles)
     # Loop through all folders
     for i, folder in tqdm(enumerate(AllFiles)):
@@ -25,7 +23,7 @@ def groundTruth(skipIndex=None):
 
         print(folder)
         # Load all scans in the folder
-        scans = os.listdir("dataset/" + str(folder) + "/Scans/")
+        scans = os.listdir("InropaFiles/" + str(folder) + "/Scans/")
 
         # Create the array that will contain the combined vertices of the stl models
         pcdArray = np.array([[0, 0, 0]])
@@ -33,7 +31,7 @@ def groundTruth(skipIndex=None):
         # Find the vertices for each stl file and save the vertices in an array
         # Concatenate the saved vertices and combined vertices array
         for x in scans:
-            foundVertices = np.asarray(o3d.io.read_triangle_mesh("Dataset/" + str(folder) + "/Scans/" + x).vertices)
+            foundVertices = np.asarray(o3d.io.read_triangle_mesh("InropaFiles/" + str(folder) + "/Scans/" + x).vertices)
             pcdArray = np.concatenate((pcdArray, foundVertices), axis=0)
 
         # Create an empty point cloud object and add the vertices to the point cloud as positions
@@ -42,10 +40,10 @@ def groundTruth(skipIndex=None):
         pcd.points = o3d.utility.Vector3dVector(pcdArray)
 
         CADPCD = np.array([[0, 0, 0]])
-        CADFiles = [f for f in listdir("Dataset/" + str(folder) + "/CADs") if isfile(join("Dataset/" + str(folder) + "/CADs", f))]
+        CADFiles = [f for f in listdir("InropaFiles/" + str(folder) + "/CADs") if isfile(join("InropaFiles/" + str(folder) + "/CADs", f))]
         for x in CADFiles:
             # Read the ground truth Cad model
-            Cad = o3d.io.read_triangle_mesh("Dataset/" + str(folder) + "/CADs/" + x)
+            Cad = o3d.io.read_triangle_mesh("InropaFiles/" + str(folder) + "/CADs/" + x)
             # Convert the Cad model into a point cloud
             CadPCD = Cad.sample_points_poisson_disk(100000, init_factor=5, pcl=None)
             CADPCD = np.concatenate((CADPCD, CadPCD.points), axis=0)
@@ -68,16 +66,15 @@ def groundTruth(skipIndex=None):
         removePer = (len(endPCD.points) / len(pcdArray)) * 100
 
         if removePer > 90:
-            o3d.io.write_point_cloud("GroundTruth/" + str(folder) + ".ply", endPCD, write_ascii=True, compressed=False, print_progress=True)
+            o3d.io.write_point_cloud("GroundTruthPCD/" + str(folder) + ".ply", endPCD, write_ascii=True, compressed=False, print_progress=True)
+            o3d.io.write_point_cloud("GroundTruthPTS/" + str(folder) + ".pts", endPCD, write_ascii=True, compressed=False, print_progress=True)
             print("point cloud and CAD saved with acurracy of " + str(removePer))
         else:
             print("point cloud and CAD discarded")
         skipIndex += 1
 
 def beamIso(pointCloud, skipIndex=None):
-    os.mkdir("CleanDataset")
-    os.mkdir("CleanPCD")
-    patchFolders = os.listdir("GroundTruth")
+    patchFolders = os.listdir("GroundTruthPCD")
     for i, folder in tqdm(enumerate(patchFolders)):
         if skipIndex >= 20:
             continue
@@ -85,30 +82,39 @@ def beamIso(pointCloud, skipIndex=None):
         try:
             # load 3D mesh
             Cad = o3d.io.read_triangle_mesh("LapperCAD/" + str(os.path.splitext(folder)[0]) + ".stl")
+            print(os.path.splitext(folder)[0])
 
             # poisson disk sampling on the loaded 3D mesh
             CadPCD = Cad.sample_points_poisson_disk(10000, init_factor=5, pcl=None)
 
             # load in a point cloud
-            pcd = o3d.io.read_point_cloud("GroundTruth/" + str(folder))
+            pcd = o3d.io.read_point_cloud("GroundTruthPCD/" + str(folder))
 
             # calculate the distance between two point, if they are the closest point set
             pcdist = np.asarray(pcd.compute_point_cloud_distance(CadPCD))
+
+            f = open(str(os.path.splitext(folder)[0]) + ".seg", "x")
+
+            for i in pcdist:
+                if i > 20:
+                    f.write(str(0) + "\n")
+                    # beam
+                else:
+                    f.write(str(1) + "\n")
+                    # patch
+
+            f.close()
             ind = np.where(pcdist > 20)[0]
             disPCD = pcd.select_by_index(ind)
 
             skipIndex += 1
-
-            # save
-            o3d.io.write_point_cloud("CleanPCD/" + str(os.path.splitext(folder)[0]) + ".ply", disPCD, write_ascii=True, compressed=False, print_progress=True)
-            o3d.io.write_point_cloud("CleanPCD/p." + str(os.path.splitext(folder)[0]) + ".ply", CadPCD, write_ascii=True, compressed=False, print_progress=True)
-            o3d.io.write_point_cloud("CleanDataset/" + str(os.path.splitext(folder)[0]) + ".pts", disPCD, write_ascii=True, compressed=False, print_progress=True)
-            o3d.io.write_point_cloud("CleanDataset/p." + str(os.path.splitext(folder)[0]) + ".pts", CadPCD, write_ascii=True, compressed=False, print_progress=True)
         except:
             # load in a point cloud
-            pcd = o3d.io.read_point_cloud("GroundTruth/" + str(folder))
-            o3d.io.write_point_cloud("CleanPCD/" + str(os.path.splitext(folder)[0]) + ".ply", pcd, write_ascii=True, compressed=False, print_progress=True)
-            o3d.io.write_point_cloud("CleanDataset/" + str(os.path.splitext(folder)[0]) + ".pts", pcd, write_ascii=True, compressed=False, print_progress=True)
+            pcd = np.asarray(o3d.io.read_point_cloud("GroundTruthPCD/" + str(folder)))
+            f = open(str(os.path.splitext(folder)[0]) + ".seg", "x")
+            for i in pcd:
+                f.write(str(0) + "\n")
+
 
 
 groundTruth(0)
